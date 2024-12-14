76ee9b7246b25a65a9274febfbeb17976a7a8a30
uwu_rimjhim <rimjhimshrivastava19@gmail.com>
2024-12-14 17:10:22 +0530

    v0.3.3: app url added

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:8080',
-      'https://bespoke-puffpuff-4433b9.netlify.app/',
-    ],
+    origin: [process.env.WEB_APP_URL, 'http://localhost:8080'],
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
