00c52d23a0a7097283e58de5a3250a0c0be36aa3
uwu_rimjhim <rimjhimshrivastava19@gmail.com>
2024-12-14 16:42:23 +0530

    v0.3.2: Added frontend link

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:8080'],
-    methods: 'GET,POST,PUT,DELETE', 
-    allowedHeaders: 'Content-Type,Authorization', 
-    credentials: true, 
-  });  
+    origin: [
+      'http://localhost:8080',
+      'https://bespoke-puffpuff-4433b9.netlify.app/',
+    ],
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
